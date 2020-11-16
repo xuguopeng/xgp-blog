59ea910387920baed33cc626751a969f00999513
新西兰的肉夹馍 <869297053@qq.com>
2020-11-16 13:33:16 +0800

    2020-11-16-02

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    JavaScript进阶: ['event-loop'],
+    说明: ['doc1', 'doc2', 'doc3'],
   },
 };
